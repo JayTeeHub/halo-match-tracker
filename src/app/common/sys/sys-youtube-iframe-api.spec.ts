7bc90cbf24a7c807a78f687adf9a6107a7a479c2
Jordan <jordanturner04@gmail.com>
2020-09-26 10:28:00 -0700

    Unit tests for YouTube iframe API  system file
    - 100% test coverage on loading the YouTube iframe  API
    - 100% test coverage on loading a video into the YouTube iframe player
    - Rewrote how the  API is loaded
    - Removed 3rd party lib for loading YouTube iframe API

--- src/app/common/sys/sys-youtube-iframe-api.spec.ts
+++ src/app/common/sys/sys-youtube-iframe-api.spec.ts
@@ -10,22 +10,119 @@ import { SysYouTubeIframeApi } from './sys-youtube-iframe-api';
  * Third-Party Dependencies
  *
  */
-import * as YouTubeIframeLoader from 'youtube-iframe';
+import { switchMap } from 'rxjs/operators';
 
 describe('SysYouTubeIframeApi', () => {
-    const mockHtmlId = `player`;
+    const mockHtmlId: string = `player`;
+    const mockWidth: string = '640';
+    const mockHeight: string = '390';
+    const mockVideoId: string = 'M7lc1UVf-VE';
+    const mockOnReadyEvent: Function = () => {};
+    const mockOnStateChangeEvent: Function = () => {};
 
-    // Global instance to be used for each test
-    beforeEach(() => {});
+    const youtubeIframeApi = 'https://www.youtube.com/iframe_api';
 
-    it('should load the YouTube API', () => {
+    it('should load the YouTube API', done => {
         // Arrange
-        spyOn(YouTubeIframeLoader, 'load').and.callThrough();
+        const sysYouTubeIframeApi = new SysYouTubeIframeApi(
+            mockHtmlId,
+            mockWidth,
+            mockHeight,
+            mockOnReadyEvent,
+            mockOnStateChangeEvent
+        );
 
         // Act
-        new SysYouTubeIframeApi(mockHtmlId);
+        sysYouTubeIframeApi.loadApi$().subscribe(() => {
+            // Assert
+            expect(<any>window['onYouTubeIframeAPIReady']).toBeDefined();
+            expect(<any>sysYouTubeIframeApi['youTubeIframe']).toBeDefined();
+            expect(document.getElementsByTagName('script').namedItem('youtubeIframe').src).toEqual(
+                youtubeIframeApi
+            );
+
+            // Complete
+            done();
+        });
+    });
+
+    it('should return right away if the YouTube API has already been loaded', done => {
+        // Arrange
+        const sysYouTubeIframeApi = new SysYouTubeIframeApi(
+            mockHtmlId,
+            mockWidth,
+            mockHeight,
+            mockOnReadyEvent,
+            mockOnStateChangeEvent
+        );
+
+        // Act
+        sysYouTubeIframeApi
+            .loadApi$()
+            .pipe(switchMap(() => sysYouTubeIframeApi.loadApi$()))
+            .subscribe(() => {
+                // Assert
+                expect(
+                    document.getElementsByTagName('script').namedItem('youtubeIframe').src
+                ).toEqual(youtubeIframeApi);
+
+                // Complete
+                done();
+            });
+    });
+
+    it('should load a YouTube video into an iframe', done => {
+        // Arrange
+        const sysYouTubeIframeApi = new SysYouTubeIframeApi(
+            mockHtmlId,
+            mockWidth,
+            mockHeight,
+            mockOnReadyEvent,
+            mockOnStateChangeEvent
+        );
+
+        // Act
+        sysYouTubeIframeApi.loadApi$().subscribe(() => {
+            // Arrange
+            spyOn(<any>sysYouTubeIframeApi['youTubeIframe'], 'Player');
+
+            // Act
+            sysYouTubeIframeApi.loadVideo(mockVideoId);
+
+            // Assert
+            expect(<any>sysYouTubeIframeApi['youTubeIframe'].Player).toHaveBeenCalledWith(
+                mockHtmlId,
+                {
+                    videoId: mockVideoId,
+                    height: mockHeight,
+                    width: mockWidth,
+                    events: {
+                        onReady: mockOnReadyEvent,
+                        onStateChange: mockOnStateChangeEvent
+                    }
+                }
+            );
+
+            // Complete
+            done();
+        });
+    });
+
+    it('should console an error when trying to load a video without loading the API', () => {
+        // Arrange
+        const sysYouTubeIframeApi = new SysYouTubeIframeApi(
+            mockHtmlId,
+            mockWidth,
+            mockHeight,
+            mockOnReadyEvent,
+            mockOnStateChangeEvent
+        );
+        spyOn(console, 'error');
+
+        // Act
+        sysYouTubeIframeApi.loadVideo(mockVideoId);
 
         // Assert
-        expect(YouTubeIframeLoader.load).toHaveBeenCalled();
+        expect(console.error).toHaveBeenCalledWith('The YouTube iframe API has not been loaded');
     });
 });
