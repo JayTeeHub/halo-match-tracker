26c4c9ec362423da5ee68f833891b0a4c2d47f05
Jordan <jordanturner04@gmail.com>
2020-10-12 11:17:04 -0700

    Changes to match tracker
    - Replaced solution for starting and stopping the match tracker with  one that  uses Subjects
    - All unit testing completed for new solution

--- src/app/common/sys/match-watch.spec.ts
+++ src/app/common/sys/match-watch.spec.ts
@@ -2,12 +2,11 @@
 import { MatchWatcher } from './match-watcher';
 
 /** Third-Party Dependencies */
-import { NEVER } from 'rxjs';
 
 describe('MatchWatcher', () => {
     const clock = jasmine.clock();
 
-    it('should increase the time elapsed on each interval of the watcher', () => {
+    it('should initialize and start the watcher the first time the start method is called', () => {
         // Arrange
         const matchWatcher = new MatchWatcher();
         const intervalPeriod = matchWatcher['watcherSpeed'];
@@ -22,33 +21,111 @@ describe('MatchWatcher', () => {
 
         // Act
         clock.tick(intervalPeriod);
+
+        // Assert
         expect(matchWatcher.time).toEqual(intervalPeriod * 2);
 
-        // Stop the interval on the watcher and stop the jasmine clock
-        matchWatcher['watcherSubscription'].unsubscribe();
         clock.uninstall();
     });
 
-    fit('should pause the watcher interval from increasing the time elapsed', () => {
+    it('should start the watcher when instance already occurs by emitting a start watcher event', () => {
         // Arrange
         const matchWatcher = new MatchWatcher();
         const intervalPeriod = matchWatcher['watcherSpeed'];
         clock.install();
 
+        spyOn(matchWatcher['startWatcher$'], 'next');
+
+        // Mock that the watcher has already started
         matchWatcher.start();
-        spyOn(matchWatcher['watcherSubscription'], 'unsubscribe');
+        clock.tick(intervalPeriod);
+
+        // Act
+        matchWatcher.start();
+        clock.tick(intervalPeriod);
+
+        // Assert
+        expect(matchWatcher['startWatcher$'].next).toHaveBeenCalled();
+        expect(matchWatcher.time).toEqual(intervalPeriod * 2);
+
+        clock.uninstall();
+    });
+
+    it('should pause the watcher interval from increasing the time elapsed', () => {
+        // Arrange
+        const matchWatcher = new MatchWatcher();
+        const intervalPeriod = matchWatcher['watcherSpeed'];
+        clock.install();
+
+        spyOn(matchWatcher['suspendWatcher$'], 'next').and.callThrough();
+
+        // Mock that the watcher has already started
+        matchWatcher.start();
         clock.tick(intervalPeriod);
 
         // Act
         matchWatcher.pause();
+
+        // Mock time passing to ensure that the pause method worked.
         clock.tick(intervalPeriod);
 
         // Assert
         expect(matchWatcher.time).toEqual(intervalPeriod);
-        expect(matchWatcher['watcherSubscription'].unsubscribe).toHaveBeenCalled();
+        expect(matchWatcher['suspendWatcher$'].next).toHaveBeenCalled();
 
-        // Stop the interval on the watcher and stop the jasmine clock
-        matchWatcher['watcherSubscription'].unsubscribe();
+        clock.uninstall();
+    });
+
+    it('should reset the match watcher time elapsed when the stop method is called', () => {
+        // Arrange
+        const matchWatcher = new MatchWatcher();
+        const intervalPeriod = matchWatcher['watcherSpeed'];
+        clock.install();
+
+        // Mock that the watcher has already started
+        matchWatcher.start();
+        clock.tick(intervalPeriod);
+
+        // Act
+        matchWatcher.stop();
+
+        // Mock time passing to ensure that the watcher is terminated.
+        clock.tick(intervalPeriod);
+
+        // Assert
+        expect(matchWatcher.time).toEqual(0);
+
+        clock.uninstall();
+    });
+
+    it('should terminate the match watcher when the stop method is called', () => {
+        // Arrange
+        const matchWatcher = new MatchWatcher();
+        const intervalPeriod = matchWatcher['watcherSpeed'];
+        clock.install();
+
+        spyOn(matchWatcher['suspendWatcher$'], 'next').and.callThrough();
+        spyOn(matchWatcher['terminateWatcher$'], 'next').and.callThrough();
+
+        // Mock that the watcher has already started
+        matchWatcher.start();
+        clock.tick(intervalPeriod);
+
+        // Arrange
+        spyOn(matchWatcher['watcherSubscription'], 'unsubscribe').and.callThrough();
+
+        // Act
+        matchWatcher.stop();
+
+        // Mock time passing to ensure that the watcher is terminated.
+        clock.tick(intervalPeriod);
+
+        // Assert
+        expect(matchWatcher['suspendWatcher$'].next).toHaveBeenCalled();
+        expect(matchWatcher['terminateWatcher$'].next).toHaveBeenCalled();
+        expect(matchWatcher['watcherSubscription'].unsubscribe).toHaveBeenCalled();
+        expect(matchWatcher['watcher$']).toEqual(null);
+
         clock.uninstall();
     });
 });
